40d3c509deda0ab6b6e57eb56e8354061f213c9d
sdolgov77 <sdolgov@inbox.ru>
2021-09-25 14:23:50 +0600

    debuggin2

--- main.go
+++ main.go
@@ -2,16 +2,18 @@ package main
 
 import (
   "fmt"
-  "os"
+  //"os"
 )
 
 func main() {
+  fmt.Print(0)
+/*
   if len(os.Args)<2{
     fmt.Print(0)
   } else if os.Args[1]=="''" {
     fmt.Print(0)
   } else {
     fmt.Print(len(os.Args)-1)
-  }
+  }*/
   os.Exit(0)
 }  
