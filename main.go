9bad8d209ee8dcb21c1d6bea66970cb6981c26be
sdolgov77 <sdolgov@inbox.ru>
2021-09-25 14:52:51 +0600

    final10

--- main.go
+++ main.go
@@ -9,11 +9,7 @@ func main() {
   if len(os.Args)<2{
     fmt.Println(0)
   } else if len(os.Args)==2 {
-    if os.Args[1]=="'ok'" { 
       fmt.Println(1)
-    } else {
-      fmt.Println(0)
-    }
   } else {
     fmt.Println(len(os.Args)-1)
   }
