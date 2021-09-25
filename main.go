ca9777e4a07c6ab152bd68d585b4deaf83efcac8
sdolgov77 <sdolgov@inbox.ru>
2021-09-25 14:59:50 +0600

    final12

--- main.go
+++ main.go
@@ -3,16 +3,18 @@ package main
 import (
   "fmt"
   "os"
+  "strings"
 )
 
 func main() {
   if len(os.Args)<2{
     fmt.Println(0)
   } else if len(os.Args)==2 {
-    if os.Args[1]=="'ok'" { 
-      fmt.Println(1)
-    } else {
+    if os.Args[1]=="''" { 
       fmt.Println(0)
+    } else {
+      strs := strings.Split(os.Args[1], " ")
+      fmt.Println(len(strs))
     }
   } else {
     fmt.Println(len(os.Args)-1)
