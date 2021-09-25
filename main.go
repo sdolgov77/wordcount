18b0fdba7b7980f6e48a4ad351d5deecac226a94
sdolgov77 <sdolgov@inbox.ru>
2021-09-25 14:17:56 +0600

    debuggin

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 )
 
 func main() {
-  if len(os.Args)<2 || os.Args[1]=="''" {
-    fmt.Println(0)
+  if len(os.Args)<2{
+    fmt.Print(0)
+  } else if os.Args[1]=="''" {
+    fmt.Print(0)
   } else {
-    fmt.Println(len(os.Args)-1)
+    fmt.Print(len(os.Args)-1)
   }
   os.Exit(0)
 }  
