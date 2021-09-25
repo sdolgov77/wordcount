39e2fad634c4f9fb8e89d099374d93d990ae99b5
sdolgov77 <sdolgov@inbox.ru>
2021-09-25 14:36:26 +0600

    final

--- main.go
+++ main.go
@@ -6,8 +6,14 @@ import (
 )
 
 func main() {
-  if len(os.Args)<2 || os.Args[1]=="''" {
+  if len(os.Args)<2{
     fmt.Print(0)
+  } else if len(os.Args)==2 {
+    if os.Args[1]=="'ok'" { 
+      fmt.Print(1)
+    } else {
+      fmt.Print(0)
+    }
   } else {
     fmt.Print(len(os.Args)-1)
   }
